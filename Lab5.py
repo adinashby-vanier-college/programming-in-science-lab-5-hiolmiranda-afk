9a3b2a66cce9f278354543fc544fa1e51683ef08
hiolmiranda-afk <hiolmiranda@gmail.com>
2026-01-08 15:44:29 -0500

    Update Lab5.py
    Implemented functions to print a hollow square, number pattern, and sum of natural numbers.

--- Lab5.py
+++ Lab5.py
@@ -1,27 +1,37 @@
-# Example for n = 5:
-# *****
-# *   *
-# *   *
-# *   *
-# *****
 def hollow_square(n):
-    return ""
 
-# 1
-# 12
-# 123
-# 1234
+    for row in range(n):
+        for col in range(n):
+            if row == 0 or row == n - 1 or col == 0 or col == n - 1:
+                print("*", end="")
+            else:
+                print(" ", end="")
+        print()
+
+print(hollow_square(5))
+
 def number_pattern(n):
-    return ""
+     result = ""
+     for row in range(1, n):
 
-# Example: For n = 5, sum = 1 + 2 + 3 + 4 + 5 = 15
+        for col in range(1, row + 1):
+            print(col, end="")
+            result += "\n"
+        print()
+print(number_pattern(5))
+
+# # Example: For n = 5, sum = 1 + 2 + 3 + 4 + 5 = 15
 def sum_of_natural_numbers(n):
-    return ""
+    for row in range(1, n):
+        print(row, end="")
 
-# Example for n = 4:
-#    *
-#   ***
-#  *****
-# *******
+        print()
+print(sum_of_natural_numbers(6))
+
+# # Example for n = 4:
+# #    *
+# #   ***
+# #  *****
+# # *******
 def centered_star_pyramid(n):
     return ""
